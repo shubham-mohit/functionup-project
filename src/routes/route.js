5738c0a5924e1ced274e6b3082b72ad86e365888
shubham-mohit <shubhammohite801@gmail.com>
2023-05-03 13:54:57 +0530

    new branch for assignment prb

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,50 +1,24 @@
+
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
-// const myUnderscore = require('underscore')
 
-router.get('/test-me', function (req, res) {
-    res.send('This should be working!')
+
+// missing array value using get W6D1
+router.get('/sol1',function(req, res){
+    let arr = [1,2,3,4,5,6,7,8,9,11]
+    let sum = arr.reduce((x,y) => x+y)
+    let n = arr.length + 1
+    let missingvalue = ((n*(n+1))/2) - sum
+    console.log('missingvalue :'+missingvalue)
 });
 
-let persons= [
-    {
-    name: "PK",
-    age: 10,
-    votingStatus: false
- },
- {
-    name: "SK",
-    age: 20,
-    votingStatus: false
- },
- {
-    name: "AA",
-    age: 70,
-    votingStatus: false
- },
- {
-    name: "SC",
-    age: 5,
-    votingStatus: false
- },
- {
-    name: "HO",
-    age: 40,
-    votingStatus: false
- }
- ]
-router.post('/voters',function(req,res){
-    let arr = []
-    let adult = req.query.age
-    for(let i=0; i<persons.length; i++){
-        if(persons[i].age >= adult){
-            persons[i].votingStatus = true
-            arr.push(persons[i])
-        }
-    }
-    res.send(arr)
-}) 
-
+router.get('/sol2',function(req, res){
+    let arr = [36,37,38,39,41,40,42,44]
+    let sum = arr.reduce((x,y) => x+y)
+    let n = arr.length +1
+    let missingvalue = ((n*(arr[0]+arr[arr.length-1]))/2) - sum
+    console.log('missingvalue :'+missingvalue)
+});
+module.exports = router;
 module.exports = router;
