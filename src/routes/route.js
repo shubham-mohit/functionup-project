10785e4949e8e8c72e3dba1e6d594ec22e78c65d
shubham-mohit <shubhammohite801@gmail.com>
2023-05-03 13:42:16 +0530

    Trying to keep it clean and for every assignment creating new branch

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,94 +8,43 @@ router.get('/test-me', function (req, res) {
     res.send('This should be working!')
 });
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+router.post('/voters',function(req,res){
+    let arr = []
+    let adult = req.query.age
+    for(let i=0; i<persons.length; i++){
+        if(persons[i].age >= adult){
+            persons[i].votingStatus = true
+            arr.push(persons[i])
+        }
+    }
+    res.send(arr)
+}) 
 
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
-})
-
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
-})
-
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
-    console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
-})
-
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
-})
-router.get('/movies', function(req, res){
-    res.send(['genius','dear comrade','sairat','ved'])
-})
-// router.get('/movies/:indexNumber',function(req ,res){
-//     const movies = ['genius','dear comrade','sairat','ved']
-//     console.log(req.params.indexNumber)
-//     let no = req.params.indexNumber;
-//     if(no > movies.length-1 || no < 0){
-//     res.send('Enter valid no')
-//     }
-// })
-router.get('/movies/:indexNumber',function(req , res){
-    const movies = ['genius','dear comrade','sairat','ved']
-    // console.log(req.params.indexNumber)
-    let a = req.params.indexNumber
-    console.log(movies[a])
-})
-router.get('/films',function(req , res){
-    res.send([ {
-        'id': 1,
-        'name': 'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-       )
-})
-router.get('/films/:filmId', function(req ,res){
-    const films = [ {
-        'id': 1,
-        'name': 'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-    console.log(req.params.filmId)
-    let b= req.params.filmId
-    console.log(films[b])
-})
 module.exports = router;
